225960d9130f88075e6a8a87f8b2b836da574809
DanyAdhi <45444857+DanyAdhi@users.noreply.github.com>
2019-01-11 08:42:42 +0700

    fix

--- application/views/m_worker/add.php
+++ application/views/m_worker/add.php
@@ -1,7 +1,4 @@
-<?php 
 
-
-?>
 <div class="content-inner">
     <div class="breadcrumb-holder">
         <div class="container-fluid">
@@ -47,23 +44,10 @@
                     <div class="form-group">
                       <div class="row">
                         <div class="col">
-                          <label><?php echo $resource['res_classid']?></label>
-                            <div class="form-group">
-                              <div class="input-group">
-                                <input type="text" name="ClassId" hidden="true" id="ClassId" placeholder="kelas" class="form-control" value="<?php echo $model['classid']?>" required>
-
-                                <input readonly="" id="groupname" placeholder="<?php echo $resource['res_name']?>" type="text" class="form-control" value="<?php echo $model['classid'] ?>">
-
-                                <div class="input-group-append">
-                                  <button id="btnGroupModal" data-toggle="modal" type="button" class="btn btn-primary" onclick="getClass(1);" data-target="#modalClass"><i class="fa fa-search"></i></button>
-                                </div>
-                              </div>
-
-                            </div>   
                       <label><?php echo $resource['res_classid']?></label>
                       <div class="input-group">
                         <input hidden="true" id = "classid" type="text" class="form-control" name = "classid" value="<?php echo $model['classid']?>">
-                        <input readonly id = "name" placeholder="<?php echo $resource['res_classid']?>" type="text" class="form-control"  value="<?php echo $model['name']?>">
+                        <input readonly id = "name" placeholder="<?php echo $resource['res_classid']?>" type="text" class="form-control"  value="<?php echo $model['classid']?>">
                         <div class="input-group-append">
                           <button id="btnGroupModal" data-toggle="modal" type="button" class="btn btn-primary" onclick="getModalGroup(1);" data-target="#modalGroupClass"><i class="fa fa-search"></i></button>
                         </div>
@@ -72,7 +56,7 @@
 
                         <div class="col">
                           <label><?php echo $resource['res_nip']?></label>
-                          <input id="nip" type="number" placeholder="<?php echo $resource['res_nip']?>" class="form-control" name = "nip" value="<?php echo $model['nip']?>">
+                          <input id="nip" type="text" placeholder="<?php echo $resource['res_nip']?>" class="form-control" name = "nip" value="<?php echo $model['nip']?>">
                         </div>
                       </div>
                     </div>
@@ -106,7 +90,7 @@
                             foreach ($enums['genderenum'] as $value)
                             { 
                             ?>
-                              <option value ="<?php echo $value->Value?>"><?php echo $value->EnumName?></option>
+                              <option value ="<?php echo $value->Value?>"><?php echo $resource[$value->Resource]?></option>
                             <?php 
                             }
                             ?>
@@ -115,8 +99,6 @@
                         </div>
                         <div class="col">
                           <label><?php echo $resource['res_religion']?></label>
-                            <select id = "religion" name="religion" class="form-control">
-                          <option value="" selected>-- Select Religion --</option>
                           
                             <select id = "religion" name="religion" class="form-control">
                               <option value="" selected>-- Select Religion --</option>
@@ -143,14 +125,10 @@
                         <div class="col">
                           <label><?php echo $resource['res_telephone']?></label>
                           <div>
-                             <input id="telephone" type="number" placeholder="<?php echo $resource['res_telephone']?>" class="form-control" name = "telephone" value="<?php echo $model['telephone']?>">                        </div>
+                             <input id="telephone" type="text" placeholder="<?php echo $resource['res_telephone']?>" class="form-control" name = "telephone" value="<?php echo $model['telephone']?>">                        </div>
                         </div>
                         <div class="col">
                           <label><?php echo $resource['res_work_status']?></label>
-                            <select id = "worker_status" name="work_status" class="form-control">
-                          <option value="" selected>-- Select Status --</option>
-
-
                             <select id = "worker_status" name="worker_status" class="form-control">
                               <option value="" selected>-- Select Worker Status --</option>
                             <?php   
@@ -177,120 +155,6 @@
         </div>
       </section>
 
-
-
-      <!-- MODAL -->
-
-
-      <div id="modalClass" tabindex="-1" role="dialog" aria-labelledby="groupUserModalLabel" aria-hidden="true" class="modal fade text-left">
-        <div role="document" class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 id="groupUserModalLabel" class="modal-title">KELAS</h5>
-              <button type="button" data-dismiss="modal" aria-label="Close" class="close"><span aria-hidden="true">×</span></button>
-            </div>
-            <div id = "cardModalBody" class="card-body">
-              <div class="form-group row">
-                <div class="col-sm-12">
-                  <div class="form-group">
-                    <div class="input-group">
-                      <input id = "searchInput" type="text" class="form-control" >
-                      <div class="input-group-append">
-                        <button id = "searchbutton" type="button" class="btn btn-primary" onclick ="getClass(1);">Search</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="table-responsive">
-                <table id = "tblClassLookUp" class="table table-striped table-hover">
-                  <thead>
-                    <tr>
-                      <th>Kelas</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-     <script type = "text/javascript">
-  $(document).ready(function() {    
-    init();
-  });
-
-  function init(){
-    <?php 
-    if($this->session->flashdata('add_warning_msg'))
-    {
-      $msg = $this->session->flashdata('add_warning_msg');
-      for($i=0 ; $i<count($msg); $i++)
-      {
-    ?>
-        setNotification("<?php echo $msg[$i]; ?>", 3, "bottom", "right");
-    <?php 
-      }
-    }
-    ?>
-  }
-
-  function getClass(page)
-  {
-    removeModalClassComponent();
-    var search = $('#searchInput').val();
-    $.ajax({
-      type: "POST",
-      url: "<?php echo base_url('M_kelas/groupClass')?>",
-      data:{
-            page: page,
-            search : search
-          },
-      success:function(data){
-        var dataClass = $.parseJSON(data);
-        console.log(dataClass);
-        setResourceModalGroupUser(dataClass['M_kelas']['resourcemodal']);
-        var detail = dataClass['M_kelas']['modeldetailmodal'];
-        for(var i = 0; i < detail.length; i++)
-        {
-          $("#tblClassLookUp").append("<tr onclick='chooseGroupName("+detail[i].Id+","+'"'+detail[i].Nama+'"'+");'><td>" + detail[i].Nama + "</td></tr>");
-        }
-
-        var previous = "";
-        var pages = "";
-        var next = "";
-        var append = "";
-        if(dataClass['M_kelas']['currentpagemodal'] > 3)
-        {
-          previous += "<li class='page-item'>";
-          previous += "<a class='page-link' href='#' onclick = 'getClass("+(dataClass['M_kelas']['currentpagemodal']-1)+")' aria-label='Previous'>";
-          previous += "<span aria-hidden='true'>&laquo;</span>";
-          previous += "<span class='sr-only'>Previous</span>";
-          previous += "</a>" ;
-          previous += "</li>";
-        }
-
-        for (var i = dataClass['M_kelas']['firstpagemodal'] ; i <= dataClass['M_kelas']['lastpagemodal']; i++){
-          pages += " <li class='page-item' >";
-          pages += "<a class='page-link' href='#' onclick = 'getClass("+i+")'>"+i+"</a>";
-          pages += "</li>";
-        }
-
-        if(dataClass['M_kelas']['currentpagemodal'] < dataClass['M_kelas']['totalpagemodal'] - 2)
-        {
-          next += "<li class='page-item'>";
-          next += "<a class='page-link' href='#' onclick = 'getClass("+(1+dataClass['M_kelas']['currentpagemodal'])+")' aria-label='Next'>";
-          next += "<span aria-hidden='true'>&raquo;</span>";
-          next += "<span class='sr-only'>Next</span>";
-          next += "</a>" ;
-          next += "</li>";
-        }
-
-        append += "<div id = 'modalGroupUserPaging' class='row'>";
 <div id="modalGroupClass" tabindex="-1" role="dialog" aria-labelledby="groupClassModalLabel" aria-hidden="true" class="modal fade text-left">
   <div role="document" class="modal-dialog">
     <div class="modal-content">
@@ -414,7 +278,6 @@
         append += "</nav>";
         append += "</div>";
         append += "<div class = 'col-lg-6 icon-custom-table-header'>";
-        append +="Total Data : "+dataClass['M_kelas']['totalrowmodal'];
         append +="Total Data : "+groupclass['m_kelas']['totalrowmodal'];
         append += "</div>";
         append += "</div>";
@@ -424,29 +287,6 @@
     });
   };
 
-  function chooseGroupName(Id, Nama)
-  {
-    $("#ClassId").val(Id);
-    $("#groupname").val(Nama);
-    $('#modalClass').modal('hide');
-  }
-
-  $("#modalClass").on('hidden.bs.modal', function(){
-    removeModalClassComponent();
-  });
-
-  function removeModalClassComponent()
-  {
-    $("#tblClassLookUp").find("tr:gt(0)").remove();
-    $("#modalGroupUserPaging").remove();
-  }
-
-  function setResourceModalGroupUser(resource)
-  {
-    $("#searchbutton").innerHtml = resource['res_search'];
-    $("#groupUserModalLabel").text = resource['res_groupuser'];
-  }
-</script>
   function chooseName(Id, Nama)
   {
     $("#classid").val(Id);
